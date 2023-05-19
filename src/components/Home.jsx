2466cfa60b303de10550c7a35641e8657133544d
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-19 22:46:33 +0530

    Add about us

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
+import Logo from '../assets/coding club dp copy 2.svg'
+import '../styles/home.css'
+import AboutUsPic from '../assets/Code typing-bro (1) 1.svg'
+
 const Home = () => {
     return(
         <div className="flex-auto">
-            Home Page
+            <div className="flex flex-col justify-center items-center relative h-screen pb-44 sm:pb-32">
+                <div className='absolute top-1/4 text-4xl sm:text-6xl'>Welcome to</div>
+                <img src={Logo} alt='logo' className='h-80 sm:h-[70vh]'></img>
+            </div>
+            <div className='relative'>
+            <img src={AboutUsPic} className='absolute right-0 z-10'></img>
+                <div className='about-us h-[120vh] bg-gradient-to-r from-white to-slate-300 text-black relative'>
+                    <div className='absolute top-32 left-9 w-[35%]'>
+                        <div className='text-4xl sm:text-5xl font-black'><span className='text-[#112EAF]'>{'<'}</span>About Us<span className='text-[#112EAF]'>{'>'}</span></div>
+                        <p className='text-xl mt-7'><span className='text-[#112EAF] font-bold'>Coding club TKMCE</span> is an initiative to inculcate the culture of programming and development in our institute. We create a platform which allows students to gain assistance and mentorship to enhance their coding ability. We ensure that every student here at TKMCE should have an opportunity to learn how to code and develop a passion for it. Our aim is to impart and instill this culture of learning-by-practice among students and professionals alike, and to that end, our team organizes a multitude of events throughout the year.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
