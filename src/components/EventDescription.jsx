cec36fb49f3c54510c32b434a15870ebf59a04a9
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-21 22:52:44 +0530

    Add events

---
+++ src/components/EventDescription.jsx
@@ -0,0 +1,11 @@
+import EventImage from '../assets/Event Card.svg'
+
+const EventDescription = () => {
+    return(
+        <div className='flex-auto'>
+            Event Description
+        </div>
+    );
+};
+
+export default EventDescription;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,18 @@
+import EventCard from "./EventCard";
+
 const Events = () => {
     return(
-        <div className="flex-auto">
-            Events Page
+        <div className="flex-auto p-7 mb-7 sm:mb-20 sm:px-14">
+            <div className="flex sm:mt-7 mb-24">
+                <div className="font-black text-4xl sm:text-6xl">EVENTS</div>
+                <div className="flex-auto border-b-4 mb-2 ml-2"></div>
+            </div>
+            <div className="flex flex-col items-center gap-24 sm:grid sm:grid-cols-3 sm:gap-36 sm:px-10">
+                <EventCard/>
+                <EventCard/>
+                <EventCard/>
+                <EventCard/>
+            </div>
         </div>
     );
 };
