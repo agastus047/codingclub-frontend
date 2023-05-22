1026ca08e1e33e9dba11a95d1b7957e5368c75e0
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-22 21:45:59 +0530

    Adjust responsiveness

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -7,7 +7,7 @@ const Events = () => {
                 <div className="font-black text-4xl sm:text-6xl">EVENTS</div>
                 <div className="flex-auto border-b-4 mb-2 ml-2"></div>
             </div>
-            <div className="flex flex-col items-center gap-24 md:grid md:grid-cols-3 md:gap-36 sm:px-10">
+            <div className="flex flex-col items-center gap-24 md:grid md:grid-cols-2 xl:grid-cols-3 md:gap-36 sm:px-10">
                 <EventCard/>
                 <EventCard/>
                 <EventCard/>
