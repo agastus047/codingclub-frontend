1aa567c73a7fba6ad7847a6b6a651a4dcfb54d73
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-26 14:52:52 +0530

    Changes

--- src/components/Execom.jsx
+++ src/components/Execom.jsx
@@ -9,7 +9,7 @@ const Execom = () => {
                 <div className="flex-auto border-b-4 mb-2 ml-2"></div>
             </div>
             <div className="flex justify-center">
-                <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full md:w-3/4 md:gap-y-16">
+                <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full md:w-[80%] xl:w-[85%] 2xl:w-3/4 md:gap-y-16">
                     <ExecomCard/>
                     <ExecomCard/>
                     <ExecomCard/>
