e57d323abaf99d7c5b57ceb4768bbd8e35938b4c
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-26 11:50:38 +0530

    Changes

--- src/components/ExecomCard.jsx
+++ src/components/ExecomCard.jsx
@@ -5,9 +5,9 @@ const ExecomCard = () => {
 
     return(
         <div className='w-52 flex flex-col'>
-            <div className='card-image w-full h-72 xl:h-56 relative overflow-hidden'>
+            <div className='card-image w-full h-56 relative overflow-hidden'>
                 <img src={ExecomPic} className='object-cover object-center w-full h-full'></img>
-                <div className='card-icon absolute h-[30%] w-[35%] bg-[#E5245A] bottom-[-30%] right-0'>
+                <div className='card-icon absolute h-[35%] w-[35%] bg-[#E5245A] bottom-[-35%] right-0'>
                     <div className='relative h-full w-full'>
                         <span className='absolute pi pi-linkedin bottom-2 right-3 text-xl hover:text-blue-950 transition-all duration-300 ease-out'></span>
                     </div>
