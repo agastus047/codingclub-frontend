96141215414dbea9decca31bff43b6b57dcd94d2
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-07-03 22:13:08 +0530

    firstcommit

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,13 +1,12 @@
 import EventPic from '../assets/Event Card.svg'
-
-const EventCard = () => {
+const EventCard = ({eventName,organizer,eventDate,venue}) => {
     return(
         <div className='border-2 border-[#DE3500] p-2 flex flex-col gap-2'>
             <img src={EventPic} className='object-center object-cover h-48 w-full'></img>
-            <div className='text-xl font-black text-center'>Computer Basics Workshop</div>
-            <div className='text-sm font-extrabold'>by Jon Doe</div>
-            <div className='font-montserrat'><span className='pi pi-calendar'></span><span className='font-black'> Event Date:</span> 21-05-2023</div>
-            <div className='font-montserrat'><span className='pi pi-map-marker'></span><span className='font-black'> Venue:</span> TKM College of Engineering</div>
+            <div className='text-xl font-black text-center'>{eventName}</div>
+            <div className='text-sm font-extrabold'>{organizer}</div>
+            <div className='font-montserrat'><span className='pi pi-calendar'></span><span className='font-black'> Event Date:</span> {eventDate}</div>
+            <div className='font-montserrat'><span className='pi pi-map-marker'></span><span className='font-black'> Venue:</span> {venue}</div>
             <div className='flex justify-center'>
                 <button type="button" className="text-white bg-[#DE3500] hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Register Here</button>
             </div>
