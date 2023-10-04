5cc1174965c6e4b73e82d6b2a6d7d841873faa36
Agastus Joyson <agastusjoyson@gmail.com>
2023-10-04 20:43:39 +0530

    add logout function

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,26 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
+import { googleLogout } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
 
-    const {userState} = useContext(UserContext);
-    const [userDetails,setUserDetails] = userState;
+    const navigate = useNavigate();
+
+    const { userState, tokenState } = useContext(UserContext);
+    const [userDetails, setUserDetails] = userState;
+    const [token, setToken] = tokenState;
+
+    const handleLogout = async () => {
+        googleLogout();
+        localStorage.setItem('CCUserToken',"");
+        setUserDetails(null);
+        setToken("");
+        navigate("/");
+    };
 
     return(
-        <div>
+        <div className="flex-auto p-7 mb-7 sm:mb-20 sm:px-14">
             {userDetails ? 
             (
                 <div>{userDetails.name},{userDetails.email}</div>
@@ -16,7 +29,8 @@ export default function Profile() {
             (
                 <div>Please log in</div>
             )
-        }
+            }
+            {userDetails && <button className="py-2 px-2 rounded-lg bg-white text-black" onClick={handleLogout}>Logout</button>}
         </div>
     );
 }
