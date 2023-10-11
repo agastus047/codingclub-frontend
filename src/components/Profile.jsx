ff3277e9dd1efad9a1e9f3d106fbe8d3733898d0
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-10-11 12:35:10 +0530

    button fixes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -52,10 +52,10 @@ const ProfilePage = () => {
             </div>
         </div>
         <div className="flex flex-col gap-5 md:flex-row md:justify-between md:gap-5">
-          <div className="p-3 sm:p-5 flex flex-row justify-center md:justify-start items-center md:items-end">
+          <div className="md:p-5 flex flex-row justify-center md:justify-start items-center md:items-end">
             <button type="button" className="text-white w-[70%] sm:w-44 bg-[#E5245A] hover:bg-[#E5244A] focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-2 text-center">Edit</button>
           </div>
-          <div className="p-3 sm:p-5 flex flex-row justify-center md:justify-start items-center md:items-end">
+          <div className="md:p-5 flex flex-row justify-center md:justify-start items-center md:items-end">
             <button type="button" className="text-white w-[70%] sm:w-44 bg-[#E5245A] hover-bg-[#E5244A] focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-2 text-center">Logout</button>
           </div>
         </div>
