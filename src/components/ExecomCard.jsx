869898704ee860caead4aad213cb57a40bcd3b87
Agastus Joyson <agastusjoyson@gmail.com>
2023-11-05 20:24:56 +0530

    update execom page

--- src/components/ExecomCard.jsx
+++ src/components/ExecomCard.jsx
@@ -6,7 +6,7 @@ const ExecomCard = ({item}) => {
     return(
         <div className='w-52 flex flex-col'>
             <div className='card-image w-full h-56 relative overflow-hidden'>
-                <img src={item.photo} className='object-cover object-center w-full h-full'></img>
+                <img src={item.image} className='object-cover object-center w-full h-full'></img>
                 <div className='card-icon absolute h-[35%] w-[35%] bg-[#E5245A] bottom-[-35%] right-0'>
                     <div className='relative h-full w-full'>
                         <span className='absolute pi pi-linkedin bottom-2 right-3 text-xl hover:text-blue-950 transition-all duration-300 ease-out'></span>
